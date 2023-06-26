a9c54816851478124d70497284e16155036dcb4a
xeon-alpha-kplr <124895383+xeon-alpha-kplr@users.noreply.github.com>
2023-06-26 12:36:14 +0000

    Ajout de la fonction de modifier_élève

--- gestion_eleves.py
+++ gestion_eleves.py
@@ -3,3 +3,12 @@ def ajouter_élève():
     nom_élève = input("Entrez le nom de l'élève : ")
     élèves[id_élève] = nom_élève
     print("Élève ajouté avec succès !")
+
+def modifier_élève():
+    id_élève = input("Entrez l'identifiant de l'élève à modifier : ")
+    if id_élève in élèves:
+        nouveau_nom = input("Entrez le nouveau nom de l'élève : ")
+        élèves[id_élève] = nouveau_nom
+        print("Élève mis à jour avec succès !")
+    else:
+        print("L'élève n'existe pas.")
